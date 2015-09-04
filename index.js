ca9196f7b2a7d2bca8dacb8075d0bde85305e7a1
Jonathan Guerrero <jonathan@practisis.com>
2015-09-04 15:56:22 -0500

    pruebas

---
+++ index.js
@@ -0,0 +1,15 @@
+function dale(){
+cordova.plugins.barcodeScanner.scan(
+	alert("vamos");
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+   
+}
